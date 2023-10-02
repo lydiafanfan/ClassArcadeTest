dcdf00f62366449cf5f6d34b672ea5c5d77abeae
lydiafanfan <94136763+lydiafanfan@users.noreply.github.com>
2023-10-02 18:20:58 -0500

    player health

--- Assets/EnemyBullet.cs
+++ Assets/EnemyBullet.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    public float speedBullet;
+    public float speedBullet = 5f; // Adjust this as needed
+    public int damage = 10; // Adjust this as needed
 
     private Transform player;
     private Vector2 target;
@@ -21,17 +22,27 @@ public class EnemyBullet : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target, speedBullet * Time.deltaTime);
-        
-        if (transform.position.x == target.x && transform.position.y == target.y){
-            Destroy(gameObject);
-        }
 
-    }
-    void OnTriggerEnter2D (Collider2D hitPlayer){
-        if (hitPlayer.CompareTag("Player")){
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
             Destroy(gameObject);
         }
     }
-    
 
+    void OnTriggerEnter2D(Collider2D hitPlayer)
+    {
+        if (hitPlayer.CompareTag("Player"))
+        {
+            // Call a method on the player's script to decrease health
+            PlayerMovement playerHealth = hitPlayer.GetComponent<PlayerMovement>();
+            
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
+
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,6 +10,18 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
     Vector2 mousePos;
 
+
+    public int health = 100;
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0){
+                Destroy(gameObject);
+            }
+        }
+
+
     // Update is called once per frame
     void Update()
     {

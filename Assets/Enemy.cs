e277a69d27cc791ef3184195079fcd3312c0ecdb
lydiafanfan <94136763+lydiafanfan@users.noreply.github.com>
2023-10-02 21:15:17 -0500

    win restart page

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    public int health = 100;
-    //public GameObject deathEffect;
-    public void TakeDamage (int damage)
+    public static int totalEnemies = 3; 
+
+    public int health = 260;
+
+
+    public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health <= 0){
+        if (health <= 0)
+        {
             Destroy(gameObject);
+            CheckWin();
         }
     }
 
-    // void Die(){
-    //     Instantiate(deathEffect, transform.position, Quaternion.identity);
-        
-    // }
+    void CheckWin()
+    {
+        totalEnemies--;
+        Debug.Log("died test");
+
+        if (totalEnemies <= 0)
+        {
+            
+            SceneManager.LoadScene("WinScreen"); 
+        }
+    }
 }
+
+   
